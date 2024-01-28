53f44531289b88cd61ad2bcafd351ae6057987d2
Holden <holden@rdstudios.org>
2024-01-27 17:34:56 -0800

    Starting World Drawing + Movement

--- game.c
+++ game.c
@@ -2,12 +2,38 @@
 
 #define SPEED 100
 
+#define WIDHT  800
+#define HEIGHT 600
+#define LEVEL_WIDTH  10
+#define LEVEL_HEIGHT 10
+
+#define TILES 2
+
+Texture2D[TILES + 1] tiles;
+
+int[LEVEL_WIDTH * LEVEL_HEIGHT] world = [
+  1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
+  2, 1, 2, 1, 2, 1, 2, 1, 2, 1,
+  1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
+  2, 1, 2, 1, 2, 1, 2, 1, 2, 1,
+  1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
+  2, 1, 2, 1, 2, 1, 2, 1, 2, 1,
+  1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
+  2, 1, 2, 1, 2, 1, 2, 1, 2, 1,
+  1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
+  2, 1, 2, 1, 2, 1, 2, 1, 2, 1,
+];
+
 Color player_color = {
   .r = 0xFF,
   .g = 0,
   .b = 0,
   .a = 0xFF,
 };
+
+void draw_world(Vector2* pos) {
+
+}
 
 void deal_with_player(Vector2* pos, float delta) {
   float sped = SPEED * delta;
@@ -21,7 +47,7 @@ void deal_with_player(Vector2* pos, float delta) {
 }
 
 int main() {
-  InitWindow(800, 600, "Game!");
+  InitWindow(WIDTH, HEIGHT, "Game!");
   Color bkg_color = {
     .r = 0,
     .b = 0,
@@ -31,10 +57,15 @@ int main() {
 
   Vector2 pos = { .x = 0.0, .y = 0.0, };
 
+  tiles[1] = LoadTexture("tile1.png");
+  tiles[2] = LoadTexture("tile2.png");
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(bkg_color);
     float delta = GetFrameTime();
+
+    draw_world(&pos);
 
     deal_with_player(&pos, delta);
 
