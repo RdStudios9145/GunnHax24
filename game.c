ddc88738abfcf89cec05357465437a1b60d03f92
Tanmay Mathur <mathurtanmay001@gmail.com>
2024-01-27 20:00:13 -0800

    updated dialogue

--- game.c
+++ game.c
@@ -39,31 +39,20 @@ Color player_color = {
 char* text[TEXT + 1] = {
   "You Should Never See This",
   // The Colonel / Player's dialogue
-  "The guests arrived at around 8:30 pm and the Cook served us dinner. 
-  All of the guests were there for dinner at the same time, and we 
-  finished at around 10 pm. Me, the poet, the merchant, and the old 
-  man went back to the living room, the butler gave us some drinks 
-  from upstairs and cleaned some rooms, and I don't know where 
-  everyone else went after that.",
+  "The guests arrived at around 8:30 pm and the Cook served us dinner. All of the guests were there for dinner at the same time, and we finished at around 10 pm. Me, the Poet, the Merchant, and the Old Man went back to the living room, the Butler gave us some drinks from upstairs and cleaned some rooms, and I don't know where everyone else went after that.",
   // The Merchant's dialogue
-  "Dinner started at 8:30 pm soon after I arrived. The cook served 
-  us dinner and we finished at 10 pm. I went back to the living room 
-  with the host and some of the other guests. After an hour or so, 
-  the host went upstairs and came back with some drinks while the 
-  butler went to clean some rooms. Soon before the butler discovered 
-  the body, the professor excused himself to use the bathroom.", 
-  // The Cook's dialogue
-  "I started preparing the meal two hours before the guests arrived 
-  at 8:30. I served courses for dinner for an hour and a half and 
-  cleaned dishes throughout dinner and after until 10:30 pm. I served 
-  some snacks for the guests afterward, and the host asked for a knife 
-  for the snacks. One of the guests returned it to me at around 11 pm 
-  as I was cleaning dishes from the snacks.",
+  "Dinner started at 8:30 pm soon after I arrived. The Cook served us dinner and we finished at 10 pm. I went back to the living room with the Host and some of the other guests. After an hour or so, the Host went upstairs and came back with some drinks while the Butler went to clean some rooms. Soon before the Butler discovered the body, the Professor excused himself to use the bathroom.", 
+  // The Butler's dialogue
+  "I was cleaning and setting up the living room before the guests arrived at 8:30 pm when we had dinner. After dinner, I was walking and chatting with the Professor. He excused himself and went upstairs to the bathroom, and I went upstairs as well to clean some rooms up there. I opened a closet, and I saw the son, slumped over and with a gash in his chest. I ran downstairs to tell everyone that the son was in a closet with a stab wound."
+  // The Old Man's dialogue
+  "I got here at around 8:30 pm around the same time as the other guests. We soon had dinner and finished at around 10 pm. I went to the living room with the Host, Merchant, and Poet. The Host got a knife for the snacks and went upstairs. After some time, he returned with some drinks. After some more talking, he asked me to return the knife from earlier to the kitchen, which I did. At around 11 pm, the Professor excused himself to go to the bathroom upstairs, and the Butler also went upstairs to clean some rooms. A few minutes later, the Butler ran downstairs, shaken, telling everyone that the Professor's son was stabbed in a closet."
   // The Professor's dialogue
-  "Oh my son, my son! Whyyyyy? I was downstairs the whole time, during
-   and after dinner, just talking with the butler near the stairs. I
-    should have never come here with my son! Why would the colonel
-     invite me to his party anyway? He hates me!"
+  "Oh my son, my son! Whyyyyy? He's only 7! We were the first guests here. The Host showed him the playroom upstairs, where he spent the whole night. I was downstairs during and after dinner, mostly talking with the Butler near the stairs. I went upstairs to the bathroom, and when I came down I was told my son was stabbed! I should have never come here with my son! Why would the colonel invite me to his party anyway? He hates me!"
+  // The Cook's dialogue
+  "I started preparing the meal two hours before the guests arrived at 8:30. I served courses for dinner for an hour and a half and cleaned dishes throughout dinner and after until 10:30 pm. I served some snacks for the guests afterward, and the Host asked for a knife for the snacks. The Old Man returned it to me at around 11 pm as I was cleaning dishes from the snacks.",
+  // The Poet's dialogue
+  "We all ate dinner at around 8:30 pm soon after we all arrived. We finished at around 10 pm and I went to the living room with some of the other guests and the Host. The Cook served us snacks and the Host got a knife to cut them into slices for us. He went upstairs to get some drinks for us. After some time, the Old Man returned the knife to the kitchen. After some more chatting, the Professor excused himself to go to the bathroom upstairs and the Butler also went to clean some rooms upstairs. After a few minutes, he ran downstairs and shouted that the Professor's son was stabbed in a closet."
+  
 };
 
 void draw_world(Vector2* pos) {
