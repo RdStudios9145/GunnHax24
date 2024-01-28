0a6c978a5bd4eef5730ae30c2e2864c01d4315d7
Holden <holden@rdstudios.org>
2024-01-27 17:14:05 -0800

    MoveMent

--- game.c
+++ game.c
@@ -1,5 +1,25 @@
 #include "./raylib/raylib.h"
 
+#define SPEED 100
+
+Color player_color = {
+  .r = 0xFF,
+  .g = 0,
+  .b = 0,
+  .a = 0xFF,
+};
+
+void deal_with_player(Vector2* pos, float delta) {
+  float sped = SPEED * delta;
+
+  if (IsKeyDown(KEY_W)) pos->y -= sped;
+  if (IsKeyDown(KEY_S)) pos->y += sped;
+  if (IsKeyDown(KEY_A)) pos->x -= sped;
+  if (IsKeyDown(KEY_D)) pos->x += sped;
+
+  DrawRectangle(pos->x, pos->y, 100, 100, player_color);
+}
+
 int main() {
   InitWindow(800, 600, "Game!");
   Color bkg_color = {
@@ -9,9 +29,15 @@ int main() {
     .a = 0xFF,
   };
 
+  Vector2 pos = { .x = 0.0, .y = 0.0, };
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(bkg_color);
+    float delta = GetFrameTime();
+
+    deal_with_player(&pos, delta);
+
     EndDrawing();
   }
 
