77889f243880d55f40289752365f83018a27685d
Holden <holden@rdstudios.org>
2024-01-28 14:50:15 -0800

    world

--- game.c
+++ game.c
@@ -28,23 +28,25 @@ Texture2D player;
 Sound walking;
 float timeSincePlayed = 0;
 
+int accused = 0;
+
 int world[LEVEL_WIDTH * LEVEL_HEIGHT] = {
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 1, 1, 1, 1, 2, 2, 2, 0, 1, 1, 1, 1, 1, 0,
-  0, 1, 1, 1, 1, 1, 2, 2, 2, 0, 1, 1, 1, 1, 1, 0,
-  0, 1, 1, 1, 1, 1, 2, 2, 2, 0, 0, 0, 0, 1, 1, 0,
-  0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
-  0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
-  0, 2, 2, 2, 2, 2, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0,
-  0, 2, 2, 2, 2, 2, 0, 2, 2, 1, 1, 1, 1, 1, 1, 0,
-  0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1, 1, 1, 0,
-  0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 1, 1, 1, 1, 0, 2, 2, 1, 1, 1, 1, 1, 1, 0,
-  0, 1, 1, 1, 1, 1, 0, 2, 2, 0, 1, 1, 1, 1, 1, 0,
-  0, 1, 1, 1, 1, 1, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 0,
-  0, 1, 1, 1, 1, 1, 0, 2, 2, 0, 1, 1, 1, 1, 1, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+  1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
+  4, 4, 4, 4, 4, 4, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
+  4, 4, 4, 4, 4, 4, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
+  4, 4, 4, 4, 4, 4, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  4, 4, 4, 4, 4, 4, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  4, 4, 4, 4, 4, 4, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+  4, 4, 4, 4, 4, 4, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
 };
 
 int npc_locations[NPCS * 2] = {
@@ -58,7 +60,7 @@ Color player_color = {
   .a = 0xFF,
 };
 
-char* names[NPCS + 2] = {
+char* names[NPCS + 1] = {
   "Diary",
   "Butler",
   "Chef",
@@ -71,32 +73,57 @@ char* names[NPCS + 2] = {
   "Colonel (You)",
 };
 
-char* text[NPCS] = {
+char* text[NPCS * 2] = {
   // The Colonel / Player's dialogue
   "Colonel (You) Diary Entry:\n\
-  The guests arrived at around 8:30 pm and the Cook served us dinner. All of the guests were there for dinner at the same time, and we finished at around 10 pm. Me, the Poet, the Merchant, and the Old Man went back to the living room, the Butler gave us some drinks from upstairs and cleaned some rooms, and I don't know where everyone else went after that.",
+  The guests arrived at around 8:30 pm and the Cook served us dinner. All of the guests were there for dinner at the same time, and we finished at around 10 pm.",
+  "Colonel (You) Diary Entry:\n\
+  The Poet, the Merchant, the Old Man and I went back to the living room, the Butler gave us some drinks from upstairs and cleaned some rooms, and I don't know where everyone else went after that.",
   // The Merchant's dialogue
   "Merchant:\n\
-  Dinner started at 8:30 pm soon after I arrived. The Cook served us dinner and we finished at 10 pm. I went back to the living room with the Host and some of the other guests. After an hour or so, the Host went upstairs and came back with some drinks while the Butler went to clean some rooms. Soon before the Butler discovered the body, the Professor excused himself to use the bathroom.", 
+  Dinner started at 8:30 pm soon after I arrived. The Cook served us dinner and we finished at 10 pm. I went back to the living room with the Host and some of the other guests.",
+  "Merchant:\n\
+  After an hour or so, the Host went upstairs and came back with some drinks while the Butler went to clean some rooms. Soon before the Butler discovered the body, the Professor excused himself to use the bathroom.", 
   // The Butler's dialogue
   "Butler:\n\
-  I was cleaning and setting up the living room before the guests arrived at 8:30 pm when we had dinner. After dinner, I was walking and chatting with the Professor. He excused himself and went upstairs to the bathroom, and I went upstairs as well to clean some rooms up there. I opened a closet, and I saw the son, slumped over and with a gash in his chest. I ran downstairs to tell everyone that the son was in a closet with a stab wound.",
+  I was cleaning and setting up the living room before the guests arrived at 8:30 pm when we had dinner. After dinner, I was walking and chatting with the Professor. He excused himself and went upstairs to the bathroom.",
+  "Butler:\n\
+  I went upstairs as well to clean some rooms up there. I opened a closet, and I saw the son, slumped over and with a gash in his chest. I ran downstairs to tell everyone that the son was in a closet with a stab wound.",
   // The Old Man's dialogue
   "Old Man:\n\
-  I got here at around 8:30 pm around the same time as the other guests. We soon had dinner and finished at around 10 pm. I went to the living room with the Host, Merchant, and Poet. The Host got a knife for the snacks and went upstairs. After some time, he returned with some drinks. After some more talking, he asked me to return the knife from earlier to the kitchen, which I did. At around 11 pm, the Professor excused himself to go to the bathroom upstairs, and the Butler also went upstairs to clean some rooms. A few minutes later, the Butler ran downstairs, shaken, telling everyone that the Professor's son was stabbed in a closet.",
+  I got here at around 8:30 pm around the same time as the other guests. We soon had dinner and finished at around 10 pm. I went to the living room with the Host, Merchant, and Poet. The Host got a knife for the snacks and went upstairs. After some time, he returned with some drinks.",
+  "Old Man:\n\
+  After some more talking, he asked me to return the knife from earlier to the kitchen, which I did. At around 11 pm, the Professor excused himself to go to the bathroom upstairs, and the Butler also went upstairs to clean some rooms. A few minutes later, the Butler ran downstairs, shaken, telling everyone that the Professor's son was stabbed in a closet.",
   // The Professor's dialogue
   "Professor:\n\
-  Oh my son, my son! Whyyyyy? He's only 7! We were the first guests here. The Host showed him the playroom upstairs, where he spent the whole night. I was downstairs during and after dinner, mostly talking with the Butler near the stairs. I went upstairs to the bathroom, and when I came down I was told my son was stabbed! I should have never come here with my son! Why would the colonel invite me to his party anyway? He hates me!",
+  Oh my son, my son! Whyyyyy? He's only 7! We were the first guests here. The Host showed him the playroom upstairs, where he spent the whole night. I was downstairs during and after dinner, mostly talking with the Butler near the stairs.",
+  "Professor:\n\
+  I went upstairs to the bathroom, and when I came down I was told my son was stabbed! I should have never come here with my son! Why would the colonel invite me to his party anyway? He hates me!",
   // The Cook's dialogue
   "Cook:\n\
-  I started preparing the meal two hours before the guests arrived at 8:30. I served courses for dinner for an hour and a half and cleaned dishes throughout dinner and after until 10:30 pm. I served some snacks for the guests afterward, and the Host asked for a knife for the snacks. The Old Man returned it to me at around 11 pm as I was cleaning dishes from the snacks.",
+  I started preparing the meal two hours before the guests arrived at 8:30. I served courses for dinner for an hour and a half and cleaned dishes throughout dinner and after until 10:30 pm.",
+  "Cook:\n\
+  I served some snacks for the guests afterward, and the Host asked for a knife for the snacks. The Old Man returned it to me at around 11 pm as I was cleaning dishes from the snacks.",
   // The Poet's dialogue
   "Poet:\n\
-  We all ate dinner at around 8:30 pm soon after we all arrived. We finished at around 10 pm and I went to the living room with some of the other guests and the Host. The Cook served us snacks and the Host got a knife to cut them into slices for us. He went upstairs to get some drinks for us. After some time, the Old Man returned the knife to the kitchen. After some more chatting, the Professor excused himself to go to the bathroom upstairs and the Butler also went to clean some rooms upstairs. After a few minutes, he ran downstairs and shouted that the Professor's son was stabbed in a closet.",
+  We all ate dinner at around 8:30 pm soon after we all arrived. We finished at around 10 pm and I went to the living room with some of the other guests and the Host. The Cook served us snacks and the Host got a knife to cut them into slices for us. He went upstairs to get some drinks for us.",
+  "Poet:\n\
+  After some time, the Old Man returned the knife to the kitchen. After some more chatting, the Professor excused himself to go to the bathroom upstairs and the Butler also went to clean some rooms upstairs. After a few minutes, he ran downstairs and shouted that the Professor's son was stabbed in a closet.",
   "*Ded LOL*",
+  "*Still Ded*",
   "Officer:\n\
   Who do you think murdered the poor kid?",
+  "Officer:\n\
+  ...\nno",
 };
+
+bool button(const char* intext, Vector2* pos, int padding, int fontsize) {
+    int width = MeasureText(intext, fontsize);
+    DrawRectangleLines(pos->x, pos->y, 2 * padding + width, 2 * padding + fontsize, WHITE);
+    DrawText(intext, pos->x + padding, pos->y + padding, fontsize, WHITE);
+    Vector2 mouse = GetMousePosition();
+    return CheckCollisionPointRec(mouse, (Rectangle){ pos->x, pos->y, 2 * padding + width, 2 * padding + fontsize}) && IsMouseButtonReleased(0);
+}
 
 void draw_world(Vector2* pos) {
   for (int i = 0; i < LEVEL_WIDTH * LEVEL_HEIGHT; i++) {
@@ -134,7 +161,7 @@ void draw_npcs(Vector2* pos) {
   }
 }
 
-void deal_with_player(Vector2* pos, float delta) {
+void deal_with_player(Vector2* pos, float delta, int talking) {
   float sped = SPEED * delta;
 
   int w = IsKeyDown(KEY_W);
@@ -147,10 +174,17 @@ void deal_with_player(Vector2* pos, float delta) {
     timeSincePlayed = 0;
   }
 
-  if (w) pos->y -= sped;
-  if (s) pos->y += sped;
-  if (a) pos->x -= sped;
-  if (d) pos->x += sped;
+  Vector2* dir = &(Vector2){ 0, 0 };
+
+  if (w) dir->y -= sped;
+  if (s) dir->y += sped;
+  if (a) dir->x -= sped;
+  if (d) dir->x += sped;
+
+  if (talking == -1 && accused == 0) {
+    pos->x += dir->x;
+    pos->y += dir->y;
+  }
 
   if (pos->x <= 0) pos->x = 0;
   if (pos->x >= (LEVEL_WIDTH - 1) * TILE_SIZE) pos->x = (LEVEL_WIDTH - 1) * TILE_SIZE;
@@ -161,31 +195,45 @@ void deal_with_player(Vector2* pos, float delta) {
   DrawTexture(player, WIDTH / 2 - 50, HEIGHT / 2 - 50, WHITE);
 }
 
-void gui(int talking) {
+void gui(int* talking) {
   char* click_text = "[Left Click] to interact";
   DrawText(click_text, WIDTH / 2 - MeasureText(click_text, 20) / 2, 0, 20, WHITE);
 
-  if (talking < 0) return;
+  if (*talking < 0) return;
 
   DrawRectangle(0, HEIGHT - TEXTBOX_HEIGHT, WIDTH, TEXTBOX_HEIGHT, BLACK);
   // DrawText(text[talking], TEXTBOX_PADDING, HEIGHT - TEXTBOX_HEIGHT + TEXTBOX_PADDING, FONTSIZE, WHITE);
-  DrawTextBoxed(GetFontDefault(), text[talking], (Rectangle){ TEXTBOX_PADDING, HEIGHT - TEXTBOX_HEIGHT + TEXTBOX_PADDING, WIDTH - 2 * TEXTBOX_PADDING, TEXTBOX_HEIGHT - 2 * TEXTBOX_PADDING }, FONTSIZE, SPACING, 1, WHITE);
+  DrawTextBoxed(GetFontDefault(), text[*talking], (Rectangle){ TEXTBOX_PADDING, HEIGHT - TEXTBOX_HEIGHT + TEXTBOX_PADDING, WIDTH - 2 * TEXTBOX_PADDING, TEXTBOX_HEIGHT - 2 * TEXTBOX_PADDING }, FONTSIZE, SPACING, 1, WHITE);
 
-  if (talking == 8) {
+  if (*talking == 16) {
     DrawRectangle(100, 50, WIDTH - 200, HEIGHT - 300, BLACK);
 
-    for (int i = 0; i < NPCS; i++) {
+    for (int i = 0; i < NPCS + 1; i++) {
       int x = 120 + (i % 2) * ((WIDTH - 200) / 2);
       int y = 120 + 40 * ((i / 2) % (int)ceil((double)NPCS / (double)2));
 
       DrawText(names[i], x, y, 20, WHITE);
+      // printf("%d\n", button("Accuse", &(Vector2){ x + 150, y - 10 }, 10, 20));
+      if (button("Accuse", &(Vector2){ x + 150, y - 10 }, 10, 20)) {
+        printf("%d\n", i);
+        if (i == 0 || i == 7 || i == 8) {
+        } else {
+          accused = i + 1;
+          *talking = -1;
+        }
+      }
     }
   }
 }
 
 void interact(int* talking, Vector2* pos) {
-  if (*talking != -1) {
+  if (*talking != -1 && *talking % 2 == 1) {
     *talking = -1;
+    return;
+  }
+
+  if (*talking != -1) {
+    *talking += 1;
     return;
   }
 
@@ -196,7 +244,7 @@ void interact(int* talking, Vector2* pos) {
     //printf("%f, %f, %d, %d, %d\n", pos->x, pos->y, x, y, i);
 
     if (abs(x - pos->x / TILE_SIZE) <= 1 && abs(y - pos->y / TILE_SIZE) <= 1) {
-      *talking = i;
+      *talking = 2 * i;
       return;
     }
   }
@@ -224,9 +272,15 @@ int main() {
   ImageResizeNN(&tile1, TILE_SIZE, TILE_SIZE);
   Image tile2 = LoadImage("tile2.png");
   ImageResizeNN(&tile2, TILE_SIZE, TILE_SIZE);
+  Image tile3 = LoadImage("Cobblestone.png");
+  ImageResizeNN(&tile3, TILE_SIZE, TILE_SIZE);
+  Image tile4 = LoadImage("WoodenPlanks.png");
+  ImageResizeNN(&tile4, TILE_SIZE, TILE_SIZE);
 
   tiles[1] = LoadTextureFromImage(tile1);
   tiles[2] = LoadTextureFromImage(tile2);
+  tiles[3] = LoadTextureFromImage(tile3);
+  tiles[4] = LoadTextureFromImage(tile4);
 
   Image book = LoadImage("diary.png");
   ImageResize(&book, TILE_SIZE, TILE_SIZE);
@@ -239,14 +293,43 @@ int main() {
   sprites[3] = LoadTexture("old_man.png");
   sprites[6] = LoadTexture("Poet.png");
   sprites[7] = LoadTexture("Son.png");
-  sprites[8] = LoadTexture("NM-Icon.png");
+  sprites[8] = LoadTexture("Police(1).png");
 
   player = LoadTexture("Colonel.png");
 
   int talking = -1;
 
   while (!WindowShouldClose()) {
-    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) interact(&talking, &pos);
+    if (accused != 0) {
+      BeginDrawing();
+
+      ClearBackground(BLACK);
+      if (accused == 10) {
+        char* line1 = "The Colonel was sent to jail.";
+        DrawText(line1, WIDTH / 2 - MeasureText(line1, 30) / 2, 50, 30, WHITE);
+
+        char* line2 = "They later confessed to the murder.";
+        DrawText(line2, WIDTH / 2 - MeasureText(line2, 20) / 2, 200, 20, WHITE);
+
+        char* line3 = "You win.";
+        DrawText(line3, WIDTH / 2 - MeasureText(line3, 20) / 2, 250, 20, WHITE);
+      } else {
+        // char* line1[200];
+        // strcat(line1, "The ");
+        // strcat(line1, names[accused - 1]);
+        // strcat(line1, " was sent to jail.");
+        // DrawText(line1, WIDTH / 2 - MeasureText(line1, 30) / 2, 50, 30, WHITE);
+
+        char* line2 = "They were not the murderer.";
+        DrawText(line2, WIDTH / 2 - MeasureText(line2, 20) / 2, 200, 20, WHITE);
+
+        char* line3 = "You lose.";
+        DrawText(line3, WIDTH / 2 - MeasureText(line3, 20) / 2, 250, 20, WHITE);
+      }
+
+      EndDrawing();
+      continue;
+    }
     BeginDrawing();
     ClearBackground(bkg_color);
     float delta = GetFrameTime();
@@ -256,9 +339,10 @@ int main() {
     draw_world(&pos);
     draw_npcs(&pos);
 
-    deal_with_player(&pos, delta);
+    deal_with_player(&pos, delta, talking);
 
-    gui(talking);
+    gui(&talking);
+    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) interact(&talking, &pos);
 
     EndDrawing();
   }
